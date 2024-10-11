0a8c0ea3564fdd7a8f16dc18d85383b279b05a56
D00262961 <d00262962@student.dkit.ie>
2024-10-11 10:36:22 +0100

    Created ca2 project, populated it with the project's requirement files.

---
+++ ca2/src/main/java/ca2/model/Event.java
@@ -0,0 +1,8 @@
+package ca2.model;
+import java.util.UUID;
+
+public class Event {
+    final static String [] VALID_SOURCES =new String[10];
+    UUID id = UUID.randomUUID();
+    String timestamp = 
+}
---
+++ ca2/target/classes/Event.java
@@ -0,0 +1,8 @@
+
+import java.util.UUID;
+
+public class Event {
+    final static String [] VALID_SOURCES =new String[10];
+    UUID id = UUID.randomUUID();
+    
+}
