87cbd98845c6debb3deb667c95ac39b0c6cdf676
Ricardo Andaluz <D00262961@student.dkit.ie>
2024-10-14 14:19:37 +0100

    Sorted merge method created.

--- deduplicationsorted/src/main/java/deduplicationsorted/mergesorted.java
+++
@@ -1,7 +0,0 @@
-package deduplicationsorted;
-
-public class mergesorted {
-    public static void main(String[] args) {
-        }
-    }
-}
--- deduplicationsorted/src/main/java/deduplicationsorted/utils/merge.java
+++ deduplicationsorted/src/main/java/deduplicationsorted/utils/merge.java
@@ -16,4 +16,29 @@ public class merge {
         return newArray;
     }
 
+    public int[] Sorted(int[] array1, int[] array2){
+        int[] newArray = new int[array1.length + array2.length];
+        int positionIndex1 = 0;
+        int positionIndex2 = 0;
+        for (int slot = 0; slot < newArray.length; slot++){
+            if (array1[positionIndex1] >= array2[positionIndex2]){
+                newArray[slot] = array1[positionIndex1++];
+            }
+            if (array2[positionIndex2] >= array1[positionIndex1]){
+                newArray[slot] = array2[positionIndex2++];
+            }
+            if (positionIndex1 > array1.length){
+                for (int num: array2){
+                    newArray[slot] = array2[num];
+                }
+            }
+            if (positionIndex2 > array2.length){
+                for (int num: array1){
+                    newArray[slot] = array1[num];
+                }
+            }
+
+        }
+        return newArray;
+    }
 }
